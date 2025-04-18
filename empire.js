8ecf8de22fea4e6659001ad8c6f37a9e5b27f3df
MarcusPrince <marukuprince@gmail.com>
2025-04-18 15:07:40 -0700

    Add files via upload

--- empire.js
+++ empire.js
@@ -1,6 +1,12 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+
+
+
+
+
+
+
+
