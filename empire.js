bf4fb53549f1deb6e8ab53e914e19d06269a29bf
MarcusPrince <marukuprince@gmail.com>
2025-04-18 22:54:32 -0700

    Add files via upload

--- empire.js
+++ empire.js
@@ -3,6 +3,16 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 
+const box = document.querySelector('.box');
+
+window.addEventListener('scroll', () => {
+  const boxPosition = box.getBoundingClientRect().top;
+  const screenPosition = window.innerHeight / 1.5;
+
+  if (boxPosition < screenPosition) {
+    box.classList.add('visible');
+  }
+});
 
 
 
